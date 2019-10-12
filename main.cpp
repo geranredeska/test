e9f1706cfaf86fc5c2ef148f3c7a92ccaebf7c82
lecomtedeshura <>
2019-10-12 15:40:41 +0700

    Edit5 main

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-<<<<<<< HEAD
-
-=======
-    git 
->>>>>>> 4cbaaf5982c2f9f9391bed063fc71f483d1eaeec
+    git commit;
     return 0;
 }
